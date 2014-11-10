54e80fc9302508432944b1447907d2250a78297c
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2014-11-10 18:27:44 -0200

    ractive backbone adaptor

--- application/initializer.js
+++ application/initializer.js
@@ -1,6 +1,10 @@
-var Boiler = require('boiler');
-
 // Código a ser executado antes da inicialização da aplicação.
+var Boiler = require('boiler'),
+	Ractive = require('ractive');
+
 require('backbone.queryparams');
+require('ractive-adaptors-backbone');
+
+Ractive.defaults.adapt.push('Backbone');
 
 Boiler.showView('#menu', 'views/menu');
