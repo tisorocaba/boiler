8e1fe9ba729c1995123e4bf50803a6cc906d5232
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2014-11-17 16:25:57 -0200

    Ajustes na task watch.

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
 
 gulp.task('watch', function() {
 	gulp.watch(['libs/*.js', '!libs/boiler.js'], ['libs']);
-	gulp.watch(['application/**/*.*', 'libs/boiler.js'], function() {
+	gulp.watch(['application/**/*.*', 'libs/boiler.js', 'config.json'], function() {
 		return runSequence(['helpers', 'components', 'partials'], 'application');
 	});
 });
