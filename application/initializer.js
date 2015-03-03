4d01fc15d43cec6957dc3025a9d0180a23d141d6
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-03-03 16:54:25 -0300

    Item ativo do menu.

--- application/initializer.js
+++ application/initializer.js
@@ -1,6 +1,12 @@
 // Código a ser executado antes da inicialização da aplicação.
-var Boiler = require('boiler');
+var Boiler = require('boiler'),
+	Marionette = require('marionette');
 
 require('backbone.queryparams');
 
 Boiler.showView('#header', require('views/menu'));
+
+// item ativo do menu
+Marionette.View.prototype.on('render', function() {
+	$('.header .nav.navbar-nav > li > a').filter('[href|="' + location.hash + '"]').parent().addClass('active');
+});
