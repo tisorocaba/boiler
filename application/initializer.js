4000d9d2cbffe2152a7f2c006f441bf2c7d28df0
Victor Bastos <victorwpbastos@gmail.com>
2015-03-30 16:29:32 -0300

    remove a classe active do item do menu antes de setar

--- application/initializer.js
+++ application/initializer.js
@@ -8,5 +8,6 @@ Boiler.showView('#header', require('views/menu'));
 
 // item ativo do menu
 Marionette.View.prototype.on('render', function() {
+	$('.header .nav.navbar-nav > li').removeClass('active');
 	$('.header .nav.navbar-nav > li > a').filter('[href|="' + location.hash + '"]').parent().addClass('active');
 });
